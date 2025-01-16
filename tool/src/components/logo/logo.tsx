0cab17c811a60f8e4f2bc230a2b05423efe1e93a
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2025-01-16 12:25:16 +0300

    favicon changed

--- tool/src/components/logo/logo.tsx
+++ tool/src/components/logo/logo.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 export interface ILogoProps {
 	onClick?: () => void;
+	className?: string;
 }
 export interface ILogoState {}
 
@@ -11,12 +12,12 @@ export default class Logo extends React.Component<ILogoProps, ILogoState> {
 	render(): ReactNode {
 		return (
 			<div
-				className="logo-container"
+				className={`logo-container ${this.props.className !== undefined?this.props.className:""}`}
 				onClick={event => {
 					if (this.props.onClick !== undefined) this.props.onClick();
 				}}>
 				<div>
-					<img src="./logo_new.svg" />
+					<img src="./exp.svg" />
 				</div>
 				<div>CoodFort</div>
 			</div>
--- tool/src/employeeApp.tsx
+++ tool/src/employeeApp.tsx
@@ -134,7 +134,7 @@ export default class EmployeeApp extends Proto<IEmployeeAppProps, IEmployeeAppSt
 
 		return (
 			<div className={this.state.exhibit === EmployeeAppExhibitViewCode.none ? "employee-app-container-notoken-none" : "employee-app-container-notoken-choosen"}>
-				<Logo
+				<Logo className="employee-app-logo"
 					onClick={() => {
 						const nState = this.state;
 						nState.exhibit = EmployeeAppExhibitViewCode.none;
